babce16391e5b74d914cefaca63c31f7debfcfa5
Joshua Tauberer <jt@occams.info>
2016-04-16 09:31:55 -0400

    tweak README, add comments to process.js, and move the 'ZZ' test to the top to simplify

--- process.js
+++ process.js
@@ -2,54 +2,73 @@ var fs = require('fs'),
     fiveColorMap = require('five-color-map'),
     turf = require('turf');
 
-var file = process.argv[2];
-
 var stateCodes = JSON.parse(fs.readFileSync('states.json', 'utf8'));
 
-var geojson = JSON.parse(fs.readFileSync(file, 'utf8'));
+// load the congressional district data
+
+var geojson = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
+// use the five-color-map package to assign color numbers to each
+// congressional district so that no two touching districts are
+// assigned the same color number
 
 var colored = fiveColorMap(geojson);
+
+// add additional metadata to the GeoJSON for rendering later and
+// compute bounding boxes for each congressional district and each
+// state so that we know how to center and zoom maps
 
 var labels = [],
     districtBboxes = {},
     stateBboxes = {};
 
 colored.features.map(function(d) {
+  // some states have district 'ZZ' which represents the area of
+  // a state, usually over water, that is not included in any
+  // congressional district --- skip these
+  if (d.properties['CD114FP'] === 'ZZ')
+    return;
 
+  // Census TIGER files have INTPTLON/INTPTLAT which conveniently
+  // provides a point where a label for the polygon can be placed.
+  // Create a turf.point to hold information for rending labels.
   var pt = turf.point([parseFloat(d.properties['INTPTLON']), parseFloat(d.properties['INTPTLAT'])]);
 
-  var number = d.properties['CD114FP'] === '00' ? 'AT LARGE' : d.properties['CD114FP'];
+  // get the district number
+  var number = d.properties['CD114FP'] === '00' ? 'At Large' : d.properties['CD114FP'];
 
+  // map the state FIPS code in the STATEFP attribute to the index of the state
+  // in our states.json file
   var state;
   stateCodes.map(function(n,i) {
     if (parseInt(d.properties['STATEFP']) === parseInt(n['Code'])) state = i;
   });
 
+  // add metadata to the label
   pt.properties = d.properties;
-
   pt.properties.title_short = stateCodes[state]['Abbr'] + ' ' + number;
   pt.properties.title_long = stateCodes[state]['State'] + ' ' + d.properties['NAMELSAD'];
+  labels.push(pt);
 
-  if (number !== 'ZZ') {
+  // collect bounding boxes for the districts
+  var bounds = turf.extent(d);
+  districtBboxes[stateCodes[state]['Abbr'] + number] = bounds;
 
-    labels.push(pt);
-
-    var bounds = turf.extent(d);
-    districtBboxes[stateCodes[state]['Abbr'] + number] = bounds;
-
-    if (stateBboxes[stateCodes[state]['Abbr']]) {
-      stateBboxes[stateCodes[state]['Abbr']].features.push(turf.bboxPolygon(bounds));
-    } else {
-      stateBboxes[stateCodes[state]['Abbr']] = { type: 'FeatureCollection', features: [] };
-      stateBboxes[stateCodes[state]['Abbr']].features.push(turf.bboxPolygon(bounds));
-    }
-
+  // and for the states
+  if (stateBboxes[stateCodes[state]['Abbr']]) {
+    stateBboxes[stateCodes[state]['Abbr']].features.push(turf.bboxPolygon(bounds));
+  } else {
+    stateBboxes[stateCodes[state]['Abbr']] = { type: 'FeatureCollection', features: [] };
+    stateBboxes[stateCodes[state]['Abbr']].features.push(turf.bboxPolygon(bounds));
   }
 });
 
+// get the bounding boxes of all of the bounding boxes for each state
 for (var s in stateBboxes) {
   stateBboxes[s] = turf.extent(stateBboxes[s]);
 }
+
+// write out data for the next steps
 
 console.log('data ready');
 
@@ -61,5 +80,4 @@ fs.writeFileSync('./website/states.js', 'var states = ' + JSON.stringify(stateCo
 var bboxes = {};
 for (var b in districtBboxes) { bboxes[b] = districtBboxes[b] };
 for (var b in stateBboxes) { bboxes[b] = stateBboxes[b] };
-
 fs.writeFileSync('./website/bboxes.js', 'var bboxes = ' + JSON.stringify(bboxes));
